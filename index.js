dd970a4c8b8b5621d4dc6dbf301bb54eb5ffbc91
Roger <rogeralemanap@gmail.com>
2023-08-21 14:31:48 -0600

    efecto bala

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function onLoad(){
+  setInterval(moverBala, 10)
+}
+
 let posicionPer=123
 
 let velosidad = 10
@@ -9,19 +13,27 @@ function clickDerecho(){
     posicionPer++
     let personaje = document.getElementById('personaje')
     personaje.style.left=posicionPer+"px"
+    
+    limite()
 }
 
 function clickIzquierdo(){
+  limite()
   posicionPer--
   let personaje = document.getElementById('personaje')
   personaje.style.left=posicionPer+"px"
+  
+  
 }
+
 
 function moverDerecha(){
   function mover(){
     posicionPer++
     let personaje = document.getElementById('personaje')
     personaje.style.left=posicionPer+"px"
+    
+    limite()
   }
   derecha =  setInterval(mover, velosidad)
 }
@@ -31,6 +43,7 @@ function moverIzquierda(){
     posicionPer--
     let personaje = document.getElementById('personaje')
     personaje.style.left=posicionPer+"px"
+    limite()
   }
   izquierda = setInterval(mover, velosidad)
 }
@@ -42,4 +55,43 @@ function detenerIzquierda(){
 
 function detenerDerecha(){
   clearInterval(derecha)
+}
+
+
+function limite(){
+  if(posicionPer==1){
+    posicionPer=2
+  }
+  else if(posicionPer==275){
+    posicionPer=274
+  }
+}
+
+
+let bala = ""
+let tope = 280
+
+function proyectil(){
+  bala= document.createElement("img")
+  bala.setAttribute("class", "bala")
+  bala.src="image/proyectil.png"
+  document.body.appendChild(bala)
+  bala.style.left=posicionPer+"px"
+}
+
+function moverBala(){
+  
+  if(tope==0){
+    tope=280
+    document.body.removeChild(bala)
+    
+  }else if(tope ==280){
+    proyectil()
+    bala.style.top=tope+"px"
+    tope-=10
+    
+  }else{
+    tope-=10
+    bala.style.top=tope+"px"
+  }
 }
